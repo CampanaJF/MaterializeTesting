ba16edf02b62019f5f725833561b5c7def0d1529
CampanaJF <jucampana@alumno.unlam.edu.ar>
2022-07-19 19:15:51 -0300

    e

---
+++ js/select.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+
+  $('.dropdown-trigger').dropdown({
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'center' // Displays dropdown with edge aligned to the left of button
+  }
+);
